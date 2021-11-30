675ae76bdc70e66902d9413f912ebf8ba793a4b5
☆ Raka ☆ ™︻®╤───────═◍➤ <93072215+Bangsat-XD@users.noreply.github.com>
2021-11-30 21:45:34 +0700

    Update Brute.py

--- Brute.py
+++ Brute.py
@@ -582,7 +582,7 @@ def sandiotomatis():
 def api(uid, pwx):
 	global ok, cp, loop, token
 	sys.stdout.write(
-		"\r %s[Crack] %s/%s OK:-%s - CP:-%s "%(N,loop, len(id), len(ok), len(cp))
+		"\r\033[1;93m◍➤ \033[0;92mCRACK \033[0;93m••>\033[0;95m %s/%s  OK:-%s - CP:-%s "%(N,loop, len(id), len(ok), len(cp))
 	); sys.stdout.flush()
 	for pw in pwx:
 		pw = pw.lower()
